2549eff46039474fcd30c28b67eee9bf048af585
Jason Duncan <functionreturnfunction@gmail.com>
2019-02-11 19:46:19 -0500

    Moved in the spike.

--- test/ert-bdd-test.el
+++ test/ert-bdd-test.el
@@ -18,49 +18,6 @@
   (-filter #'is-testp (mapcar #'identity obarray)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;UNIT TESTS;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(describe "ert-bdd-add-to-setup-stack"
-  (it "adds the body to the stack with the proper key"
-    (let (ert-bdd-description-stack ert-bdd-before-stack)
-      (setq ert-bdd-description-stack '("foo"))
-      (ert-bdd-add-to-setup-stack 'ert-bdd-before-stack '(foo))
-
-      (should (equal '(("foo" (foo))) ert-bdd-before-stack))
-
-      (setq ert-bdd-description-stack '("bar" "foo"))
-      (ert-bdd-add-to-setup-stack 'ert-bdd-before-stack '(bar))
-
-      (should (equal '(("foo" (foo)) ("foo*bar" (bar))) ert-bdd-before-stack))
-
-      (setq ert-bdd-description-stack '("baz" "bar" "foo"))
-      (ert-bdd-add-to-setup-stack 'ert-bdd-before-stack '(baz))
-
-      (should (equal '(("foo" (foo)) ("foo*bar" (bar)) ("foo*bar*baz" (baz)))
-                     ert-bdd-before-stack)))))
-
-(describe "ert-bdd-build-test-body"
-  (it "builds the test with before and after statements in the proper order"
-    (let* ((stack '(("foo" (foo))
-                    ("foo*bar" (bar))
-                    ("foo*bar*baz" (baz))))
-           (ert-bdd-before-stack stack)
-           (ert-bdd-after-stack stack)
-           (ert-bdd-description-stack '("baz" "bar" "foo")))
-      (should (equal '((foo) (bar) (baz) (test) (baz) (bar) (foo))
-                     (ert-bdd-build-test-body '((test))))))))
-
-(describe "ert-bdd-build-setup-stack"
-  (it "builds the setup stack for relevant descriptions in the proper order"
-    (let* ((stack '(("foo*bar" (bar))
-                    ("foo" (foo))
-                    ("foo*bar*baz" (baz))
-                    ("not*releveant" (yuck))))
-           (ert-bdd-description-stack '("baz" "bar" "foo"))
-           (result (ert-bdd-build-setup-stack stack)))
-      (should (equal '((foo) (bar) (baz)) result)))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;API TESTS;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (describe "describe"
@@ -75,11 +32,11 @@
 
 (let (foo)
   (describe "before"
-    (before
-     (setq foo "foo"))
+    (before :each
+      (setq foo "foo"))
 
-    (after
-     (setq foo nil))
+    (after :each
+      (setq foo nil))
 
     (it "runs before tests"
       (should (string-equal foo "foo")))
@@ -88,8 +45,8 @@
       (should (string-equal foo "foo")))
 
     (describe "when nested"
-      (before
-       (setq foo "bar"))
+      (before :each
+        (setq foo "bar"))
 
       (it "applies in the correct order"
         (should (string-equal foo "bar"))))))
